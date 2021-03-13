e83fbc255af1f6cbdd7a18c45d64fa369ae0af2c
Phongpisut Meemuk <me.phongpisut_st@tni.ac.th>
2021-03-13 15:36:29 +0700

    v1.0

--- FirstPage.py
+++ FirstPage.py
@@ -3,47 +3,105 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 import pymongo
 import pandas as pd
-import matplotlib as plt
 
 
 myserver = "mongodb+srv://admin:1234@cluster0.7voii.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority"
 
-
 class Ui_Dialog(object):
+    def __init__(self):
+        self.login = False
+
 
     def setupUi(self, Dialog):
         Dialog.setObjectName("Dialog")
-        Dialog.resize(577, 414)
-        self.dateEdit = QtWidgets.QDateEdit(Dialog)
-        self.dateEdit.setGeometry(QtCore.QRect(20, 70, 110, 22))
-        self.dateEdit.setObjectName("dateEdit")
-        self.pushButton = QtWidgets.QPushButton(Dialog)
-        self.pushButton.setGeometry(QtCore.QRect(20, 220, 111, 31))
-        self.pushButton.setObjectName("pushButton")
-        self.pushButton_2 = QtWidgets.QPushButton(Dialog)
-        self.pushButton_2.setGeometry(QtCore.QRect(20, 260, 111, 21))
-        self.pushButton_2.setObjectName("pushButton_2")
-        self.label = QtWidgets.QLabel(Dialog)
-        self.label.setGeometry(QtCore.QRect(20, 40, 47, 13))
-        self.label.setObjectName("label")
-        self.label_2 = QtWidgets.QLabel(Dialog)
-        self.label_2.setGeometry(QtCore.QRect(20, 110, 47, 13))
-        self.label_2.setObjectName("label_2")
-        self.label_3 = QtWidgets.QLabel(Dialog)
-        self.label_3.setGeometry(QtCore.QRect(20, 160, 47, 13))
-        self.label_3.setObjectName("label_3")
-        self.lineEdit_2 = QtWidgets.QLineEdit(Dialog)
-        self.lineEdit_2.setGeometry(QtCore.QRect(20, 180, 113, 20))
-        self.lineEdit_2.setObjectName("lineEdit_2")
-        self.comboBox = QtWidgets.QComboBox(Dialog)
-        self.comboBox.setGeometry(QtCore.QRect(20, 130, 111, 22))
-        self.comboBox.setObjectName("comboBox")
+        Dialog.resize(634, 354)
+        Dialog.setWindowIcon(QtGui.QIcon('/src/python.png'))
+        self.gridLayout_7 = QtWidgets.QGridLayout(Dialog)
+        self.gridLayout_7.setObjectName("gridLayout_7")
+        self.gridLayout_3 = QtWidgets.QGridLayout()
+        self.gridLayout_3.setObjectName("gridLayout_3")
         self.tableWidget = QtWidgets.QTableWidget(Dialog)
-        self.tableWidget.setGeometry(QtCore.QRect(150, 10, 411, 381))
         self.tableWidget.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)
         self.tableWidget.setObjectName("tableWidget")
         self.tableWidget.setColumnCount(0)
         self.tableWidget.setRowCount(0)
+        self.gridLayout_3.addWidget(self.tableWidget, 0, 3, 1, 1)
+        self.groupBox = QtWidgets.QGroupBox(Dialog)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.groupBox.sizePolicy().hasHeightForWidth())
+        self.groupBox.setSizePolicy(sizePolicy)
+        self.groupBox.setMinimumSize(QtCore.QSize(190, 250))
+        self.groupBox.setAlignment(QtCore.Qt.AlignLeading|QtCore.Qt.AlignLeft|QtCore.Qt.AlignTop)
+        self.groupBox.setFlat(False)
+        self.groupBox.setObjectName("groupBox")
+        self.pushButton_2 = QtWidgets.QPushButton(self.groupBox)
+        self.pushButton_2.setGeometry(QtCore.QRect(100, 180, 81, 31))
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.pushButton_2.sizePolicy().hasHeightForWidth())
+        self.pushButton_2.setSizePolicy(sizePolicy)
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.pushButton = QtWidgets.QPushButton(self.groupBox)
+        self.pushButton.setGeometry(QtCore.QRect(20, 180, 81, 31))
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.pushButton.sizePolicy().hasHeightForWidth())
+        self.pushButton.setSizePolicy(sizePolicy)
+        self.pushButton.setObjectName("pushButton")
+        self.lineEdit_2 = QtWidgets.QLineEdit(self.groupBox)
+        self.lineEdit_2.setGeometry(QtCore.QRect(20, 140, 161, 20))
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.lineEdit_2.sizePolicy().hasHeightForWidth())
+        self.lineEdit_2.setSizePolicy(sizePolicy)
+        self.lineEdit_2.setObjectName("lineEdit_2")
+        self.label_3 = QtWidgets.QLabel(self.groupBox)
+        self.label_3.setGeometry(QtCore.QRect(20, 120, 23, 13))
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.label_3.sizePolicy().hasHeightForWidth())
+        self.label_3.setSizePolicy(sizePolicy)
+        self.label_3.setObjectName("label_3")
+        self.comboBox = QtWidgets.QComboBox(self.groupBox)
+        self.comboBox.setGeometry(QtCore.QRect(20, 90, 161, 20))
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.comboBox.sizePolicy().hasHeightForWidth())
+        self.comboBox.setSizePolicy(sizePolicy)
+        self.comboBox.setObjectName("comboBox")
+        self.label_2 = QtWidgets.QLabel(self.groupBox)
+        self.label_2.setGeometry(QtCore.QRect(20, 70, 24, 13))
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.label_2.sizePolicy().hasHeightForWidth())
+        self.label_2.setSizePolicy(sizePolicy)
+        self.label_2.setObjectName("label_2")
+        self.dateEdit = QtWidgets.QDateEdit(self.groupBox)
+        self.dateEdit.setGeometry(QtCore.QRect(20, 40, 161, 20))
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.dateEdit.sizePolicy().hasHeightForWidth())
+        self.dateEdit.setSizePolicy(sizePolicy)
+        self.dateEdit.setObjectName("dateEdit")
+        self.label = QtWidgets.QLabel(self.groupBox)
+        self.label.setGeometry(QtCore.QRect(20, 20, 23, 13))
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.label.sizePolicy().hasHeightForWidth())
+        self.label.setSizePolicy(sizePolicy)
+        self.label.setObjectName("label")
+        self.gridLayout_3.addWidget(self.groupBox, 0, 1, 1, 1)
+        self.gridLayout_7.addLayout(self.gridLayout_3, 0, 1, 1, 1)
 
         self.retranslateUi(Dialog)
         QtCore.QMetaObject.connectSlotsByName(Dialog)
@@ -58,51 +116,86 @@ class Ui_Dialog(object):
 
     def retranslateUi(self, Dialog):
         _translate = QtCore.QCoreApplication.translate
-        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
-        self.pushButton.setText(_translate("Dialog", "ADD/UPDATE"))
+        Dialog.setWindowTitle(_translate("Dialog", "Revenue Reporter - Main Menu"))
+        self.groupBox.setTitle(_translate("Dialog", "Menu"))
         self.pushButton_2.setText(_translate("Dialog", "VIEW REPORT"))
-        self.label.setText(_translate("Dialog", "Date"))
-        self.label_2.setText(_translate("Dialog", "Type"))
+        self.pushButton.setText(_translate("Dialog", "ADD/UPDATE"))
         self.label_3.setText(_translate("Dialog", "Price"))
-        self.pushButton.clicked.connect(self.AddtoDB)
-
-
+        self.label_2.setText(_translate("Dialog", "Type"))
+        self.label.setText(_translate("Dialog", "Date"))
 
 
 
     def AddtoDB(self):
-        with pymongo.MongoClient(myserver) as conn:
-            try:
-                db = conn.get_database("PyProject")
-                found = db.Revenue.count_documents({"Date": self.dateEdit.dateTime().toPyDateTime()})
-                if found > 0:
-                    db.Revenue.update_one({"Date":self.dateEdit.dateTime().toPyDateTime()},{"$set":{
-                        self.comboBox.currentText():float(self.lineEdit_2.text())
-                    }})
-                    print(self.comboBox.currentText())
-                    print(float(self.lineEdit_2.text()))
-                    print("Update")
-                else:
-                    db.Revenue.insert_one({})
-                    print("insert")
 
-                self.Display()
+        if not self.lineEdit_2.text().isnumeric():
+            self.lineEdit_2.setText("0")
 
-            except Exception as e:
-                print(e)
+        if self.login:
+            with pymongo.MongoClient(myserver) as conn:
+                try:
+                    db = conn.get_database("PyProject")
+                    found = db.Revenue.count_documents({"Date": self.dateEdit.dateTime().toPyDateTime()})
+                    if found > 0:
+                        db.Revenue.update_one({"Date": self.dateEdit.dateTime().toPyDateTime()}, {"$set": {
+                            self.comboBox.currentText(): float(self.lineEdit_2.text())
+                        }})
+                    else:
+                        insert_data = {"Date": self.dateEdit.dateTime().toPyDateTime()}
+                        for i in range(self.comboBox.count()):
+                            if self.comboBox.itemText(i) == self.comboBox.currentText():
+                                insert_data = {**insert_data, self.comboBox.itemText(i): float(self.lineEdit_2.text())}
+                            else:
+                                insert_data = {**insert_data, self.comboBox.itemText(i): 0}
+                        try:
+                            db.Revenue.insert_one(insert_data)
+                        except Exception as e:
+                            print("Data Incorrect")
+
+                    msg = QtWidgets.QMessageBox()
+                    msg.setIcon(QtWidgets.QMessageBox.Information)
+                    msg.setWindowTitle("Success!")
+                    msg.setText("Process Complete!")
+                    msg.setStandardButtons(QtWidgets.QMessageBox.Ok)
+                    msg.exec_()
+
+                    try:
+                        self.Display()
+                    except Exception as e:
+                        print(e)
+
+
+                except Exception as e:
+                    msg = QtWidgets.QMessageBox()
+                    msg.setIcon(QtWidgets.QMessageBox.Warning)
+                    msg.setWindowTitle("Error!")
+                    msg.setText(e)
+                    msg.setStandardButtons(QtWidgets.QMessageBox.Ok)
+                    msg.exec_()
+
+        else:
+            msg = QtWidgets.QMessageBox()
+            msg.setIcon(QtWidgets.QMessageBox.Warning)
+            msg.setWindowTitle("Error!")
+            msg.setText("Please Login!")
+            msg.setStandardButtons(QtWidgets.QMessageBox.Ok)
+            msg.exec_()
+
+
+
+
 
 
 
     def Display(self):
 
         self.revenue = {'Date': [],
-                'Toys': [],
-                'Foods':[],
-                'Furniture':[],
-                'Clothes':[],
-                'Electronics':[]
-                }
-
+                        'Toys': [],
+                        'Foods': [],
+                        'Furniture': [],
+                        'Clothes': [],
+                        'Electronics': []
+                        }
 
         with pymongo.MongoClient(myserver) as conn:
             db = conn.get_database("PyProject")
@@ -115,7 +208,6 @@ class Ui_Dialog(object):
             for i, v in enumerate(cur):
                 self.tableWidget.setItem(i, 0, QtWidgets.QTableWidgetItem(str(v['Date'].date())))
                 self.revenue['Date'].append(str(v['Date'].date()))
-                print(v['Date'])
                 self.tableWidget.setItem(i, 1, QtWidgets.QTableWidgetItem(str(v['Toys'])))
                 self.revenue['Toys'].append(v['Toys'])
                 self.tableWidget.setItem(i, 2, QtWidgets.QTableWidgetItem(str(v['Foods'])))
@@ -127,17 +219,14 @@ class Ui_Dialog(object):
                 self.tableWidget.setItem(i, 5, QtWidgets.QTableWidgetItem(str(v['Electronics'])))
                 self.revenue['Electronics'].append(v['Electronics'])
 
-            df = pd.DataFrame(self.revenue,columns=['Date','Toys','Furniture','Clothes','Electronics'])
-
+            df = pd.DataFrame(self.revenue, columns=['Date', 'Toys', 'Foods', 'Furniture', 'Clothes', 'Electronics'])
             year = df.Date.str[0:4]
-
-            sum = df.groupby(year)['Clothes'].sum()
-            print(sum)
-
-            plot = df.plot()
+            self.report_sum = df.groupby(year)[['Toys', 'Foods', 'Furniture', 'Clothes', 'Electronics']].sum()
+            self.report_sum = self.report_sum.reset_index(level=['Date'])
+            plot = self.report_sum.plot.bar(rot=0,x='Date')
+            plot.set_xlabel("Years")
             fig = plot.get_figure()
-            fig.savefig("output.png")
-
+            fig.savefig("src/output.png")
 
 
 
@@ -154,10 +243,6 @@ class Ui_Dialog(object):
         self.tableWidget.setHorizontalHeaderItem(3, header4)
         self.tableWidget.setHorizontalHeaderItem(4, header5)
         self.tableWidget.setHorizontalHeaderItem(5, header6)
-
-
-
-
 
 
 
--- main.py
+++ main.py
@@ -1,8 +1,105 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
-import sys
+import os
+from dotenv import load_dotenv
+import hashlib
+import binascii
 
 from FirstPage import Ui_Dialog
 from Report import Report_Dialog
+from Predict import Ui_Dialog as Predict_Dialog
+from login import Ui_Dialog as Login_Dialog
+
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+import pymongo
+
+myserver = "mongodb+srv://admin:1234@cluster0.7voii.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority"
+load_dotenv(dotenv_path='.env')
+salt = os.getenv("SALT")
+
+
+class Login(QtWidgets.QDialog):
+    def __init__(self, parent=None):
+
+        super(Login,self).__init__(parent)
+        self.ui = Login_Dialog()
+
+        self.ui.setupUi(self)
+        self.oldPos = self.pos()
+
+        self.setWindowIcon(QtGui.QIcon('src/python.png'))
+        self.ui.pushButton.clicked.connect(self.trylogin)
+
+
+
+    def close(self):
+        self.hide()
+
+    def center(self):
+        qr = self.frameGeometry()
+        cp = QtWidgets.QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
+
+    def mousePressEvent(self, event):
+        self.oldPos = event.globalPos()
+
+    def mouseMoveEvent(self, event):
+        delta = QtCore.QPoint(event.globalPos() - self.oldPos)
+        self.move(self.x() + delta.x(), self.y() + delta.y())
+        self.oldPos = event.globalPos()
+
+    def trylogin(self):
+        with pymongo.MongoClient(myserver) as conn:
+            try:
+                db = conn.get_database("PyProject")
+                found = db.Users.count_documents({"Username":self.ui.lineEdit.text()})
+
+                if found > 0:
+                    users = {}
+                    cur = db.Users.find_one({"Username":self.ui.lineEdit.text()})
+                    try:
+                        newkey = hashlib.pbkdf2_hmac('sha256', self.ui.lineEdit_2.text().encode('utf-8'), salt.encode(), 100000)
+                        if cur['Password'] == binascii.hexlify(newkey).decode():
+                            msg = QtWidgets.QMessageBox()
+                            msg.setIcon(QtWidgets.QMessageBox.Information)
+                            msg.setWindowTitle("Success")
+                            msg.setText(f"Login Complete! - Welcome {cur['Username']}")
+                            msg.setStandardButtons(QtWidgets.QMessageBox.Ok)
+                            msg.exec_()
+
+                            self.parent().ui.login = True
+                            self.hide()
+
+                        else:
+                            msg = QtWidgets.QMessageBox()
+                            msg.setIcon(QtWidgets.QMessageBox.Warning)
+                            msg.setWindowTitle("Warnning")
+                            msg.setText("Username / Password Incorrect")
+                            msg.setStandardButtons(QtWidgets.QMessageBox.Ok)
+                            msg.exec_()
+
+                    except Exception as e:
+                        print(e)
+
+                else:
+                    msg = QtWidgets.QMessageBox()
+                    msg.setIcon(QtWidgets.QMessageBox.Warning)
+                    msg.setWindowTitle("Warnning")
+                    msg.setText("Username / Password Incorrect")
+                    msg.setStandardButtons(QtWidgets.QMessageBox.Ok)
+                    msg.exec_()
+
+            except Exception as e:
+                msg = QtWidgets.QMessageBox()
+                msg.setIcon(QtWidgets.QMessageBox.Warning)
+                msg.setWindowTitle("Error")
+                msg.setText(e)
+                msg.setStandardButtons(QtWidgets.QMessageBox.Ok)
+                msg.exec_()
+
 
 
 
@@ -12,12 +109,25 @@ class Main(QtWidgets.QDialog):
         super(Main,self).__init__(parent)
         self.ui = Ui_Dialog()
         self.ui.setupUi(self)
+        self.setWindowIcon(QtGui.QIcon('src/python.png'))
+        self.gotoLogin = Login(self)
 
+        self.ui.pushButton.clicked.connect(self.checkLogin)
         self.ui.pushButton_2.clicked.connect(self.clicked)
         self.report = Report(self)
 
+    def checkLogin(self):
+        if self.ui.login:
+            self.ui.AddtoDB()
+        else:
+            self.gotoLogin.show()
+
+
+
+
     def clicked(self):
-        self.report.ui.label.setPixmap(QtGui.QPixmap("output.png"))
+
+        self.report.ui.label.setPixmap(QtGui.QPixmap("src/output.png"))
         self.report.show()
 
 
@@ -26,13 +136,167 @@ class Report(QtWidgets.QDialog):
     def __init__(self,parent=None):
         super(Report,self).__init__(parent)
         self.ui = Report_Dialog()
+        self.predict = Predict(self)
+
         self.ui.setupUi(self)
+        self.ui.pushButton_2.clicked.connect(self.export_to_csv)
+        self.ui.pushButton_3.clicked.connect(self.OpenPredict)
+
+    def OpenPredict(self):
+        self.predict.show()
+
+
+
+
+
+    def export_to_csv(self):
+        try:
+           self.parent().ui.report_sum.reset_index().to_csv('output/output.csv')
+           msg = QtWidgets.QMessageBox()
+           msg.setIcon(QtWidgets.QMessageBox.Information)
+           msg.setWindowTitle("Success!")
+           msg.setText("CSV file export complete")
+           msg.setInformativeText("Please check in output folder [output.csv]")
+           msg.setStandardButtons(QtWidgets.QMessageBox.Ok)
+           msg.exec_()
+
+
+        except Exception as e:
+            print(e)
+
+
+class Predict(QtWidgets.QDialog):
+
+
+    def __init__(self,parent=None):
+        super(Predict,self).__init__(parent)
+        self.ui = Predict_Dialog()
+        self.ui.setupUi(self)
+        self.report_sum = self.parent().parent().ui.report_sum
+        self.ui.comboBox.currentTextChanged.connect(self.Show)
+        self.ui.pushButton_2.clicked.connect(self.to_csv)
+
+        for i in self.report_sum.columns:
+            if i != "Date":
+                self.ui.comboBox.addItem(i)
+
+
+        self.Show()
+
+
+
+    def Show(self):
+        try:
+            X = pd.to_numeric(self.report_sum['Date']).values.reshape(-1, 1)
+            y = self.report_sum[self.ui.comboBox.currentText()].values.reshape(-1, 1)
+
+            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+            reg = LinearRegression()
+            reg.fit(X_train, y_train)
+            y_pred = reg.predict(X_test)
+            plt.figure(figsize=(5,5),dpi=100)
+            plt.scatter(X, y, c='black')
+
+            plt.plot(
+                X_test,
+                y_pred,
+                c='blue',
+                linewidth=2
+            )
+            plt.xlabel("Year")
+            plt.ylabel("Revenue")
+
+
+            plt.savefig("src/predict.png")
+
+            self.ui.label_2.setText("Cofficeint : {0:.2f}".format(reg.coef_[0][0]))
+
+            if reg.coef_[0][0] < 0 :
+                self.ui.label_3.setText(f"Loss")
+                self.ui.label_3.setStyleSheet("color: rgb(255, 255, 255);\n"
+"background-color: rgb(255, 102, 0);")
+            if reg.coef_[0][0] < -1:
+                self.ui.label_3.setText(f"Very Loss")
+                self.ui.label_3.setStyleSheet("color: rgb(255, 255, 255);\n"
+                                              "background-color: rgb(255, 0, 0);")
+            if reg.coef_[0][0] == 0:
+                self.ui.label_3.setText(f"Natural")
+                self.ui.label_3.setStyleSheet("color: rgb(0, 0, 0);\n"
+                                              "background-color: rgb(255, 204, 153);")
+            if reg.coef_[0][0] > 0:
+                self.ui.label_3.setText(f"Slightly Gain ")
+                self.ui.label_3.setStyleSheet("color: rgb(255, 255, 255);\n"
+                                              "background-color: rgb(51, 204, 51);")
+            if reg.coef_[0][0] > 0.7:
+                self.ui.label_3.setText(f"Well Gain ")
+                self.ui.label_3.setStyleSheet("color: rgb(255, 255, 255);\n"
+                                              "background-color: rgb(0, 102, 0);")
+
+
+
+            self.ui.label.setPixmap(QtGui.QPixmap("src/predict.png"))
+
+
+        except Exception as e :
+            print(e)
+
+    def to_csv(self):
+
+        report = {"Products":[],"Status":[],"Suggestion":[]}
+
+        for i in self.report_sum.columns:
+            if i != "Date":
+                report['Products'].append(i)
+                X = pd.to_numeric(self.report_sum['Date']).values.reshape(-1, 1)
+                y = self.report_sum[i].values.reshape(-1, 1)
+
+                X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+                reg = LinearRegression()
+                reg.fit(X_train, y_train)
+                y_pred = reg.predict(X_test)
+
+                status = ""
+                sug = ""
+
+                if reg.coef_[0][0] < 0:
+                   status = "Loss"
+                   sug = "Be aware in selling it next year"
+
+                if reg.coef_[0][0] < -1:
+                   status = "Very Loss"
+                   sug = "Please Consider to cut loss"
+                if reg.coef_[0][0] == 0:
+                    status = "Natural"
+                    sug = "Nothing to worry yet"
+                if reg.coef_[0][0] > 0:
+                    status = "Slightly Gain"
+                    sug = "Please Consider to stock this product"
+
+                if reg.coef_[0][0] > 0.7:
+                    status = "Well Gain"
+                    sug = "This product might make profit as well next year"
+
+                report['Status'].append(status)
+                report['Suggestion'].append(sug)
+
+                df = pd.DataFrame(report, columns=['Products', 'Status','Suggestion'])
+
+        df.reset_index().to_csv('output/predict.csv')
+        msg = QtWidgets.QMessageBox()
+        msg.setIcon(QtWidgets.QMessageBox.Information)
+        msg.setWindowTitle("Success!")
+        msg.setText("CSV file export complete")
+        msg.setInformativeText("Please check in output folder [predict.csv]")
+        msg.setStandardButtons(QtWidgets.QMessageBox.Ok)
+        msg.exec_()
+
 
 
 
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication([])
+    app.setStyle('Fusion')
     menu = Main()
     menu.show()
     app.exec_()
